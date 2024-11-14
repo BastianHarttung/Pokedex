ebac99d2a6908ef9c26191016b30ec84d6483d84
BastianHarttung <info@harttung-fotografie.de>
2024-11-14 22:42:28 +0100

    feat(Footer): add github link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,22 @@
 import './Footer.scss';
+import { FaGithub } from "react-icons/fa";
 
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="copyright">
-        <div>© by Bastian Harttung</div>
-        <a href="https://pokeapi.co/" target="_blank">API by Pokeapi.co</a>
+      <div className="footer_content">
+        <div className="copyright">
+          <a href="https://pokeapi.co/" target="_blank">API by Pokeapi.co</a>
+        </div>
+
+        <div className="copyright">
+          <div>© by Bastian Harttung</div>
+          <a href="https://github.com/BastianHarttung"
+             target="_blank">
+            <FaGithub size={24}/>
+          </a>
+        </div>
       </div>
     </footer>
   );
