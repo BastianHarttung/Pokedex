0d6bcff83a88bc68edc486abae6b7eaaf7348569
Bastian Harttung <[bastian.harttung@externals.adidas.com]>
2024-11-18 10:01:52 +0100

    feat(App): style fixes + save pokemonIds

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss';
 import { useState, useEffect, ChangeEvent } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
-import { Pokemon, NamedAPIResourceList } from 'pokenode-ts';
+import { Pokemon, NamedAPIResourceList, NamedAPIResource } from 'pokenode-ts';
 import Pokeball from './assets/images/favicon_pokeball.png';
 import PokemonCard from './components/PokemonCard/PokemonCard.tsx';
 import Header from './components/Header/Header.tsx';
@@ -9,21 +9,22 @@ import { fetchPokemonByName, fetchPokemonList, fetchPokemonsById } from './api/p
 import Footer from './components/Footer/Footer.tsx';
 import Pokedex from './components/Pokedex/Pokedex.tsx';
 import Search from './components/Search/Search.tsx';
-import { Sorting } from "./models/enums.ts";
+import { Sorting } from './models/enums.ts';
 
 
 const sortingOptions = [
-  {value: Sorting.ID, label: "ID #"},
-  {value: Sorting.NAME, label: "Name"},
-  {value: Sorting.BASE_EXPERIENCE, label: "Erfahrung"},
-  {value: Sorting.ORDER, label: "Reihenfolge"},
-  {value: Sorting.HEIGHT, label: "Größe"},
-  {value: Sorting.WEIGHT, label: "Gewicht"},
+  {value: Sorting.ID, label: 'ID #'},
+  {value: Sorting.NAME, label: 'Name'},
+  {value: Sorting.BASE_EXPERIENCE, label: 'Erfahrung'},
+  {value: Sorting.ORDER, label: 'Reihenfolge'},
+  {value: Sorting.HEIGHT, label: 'Größe'},
+  {value: Sorting.WEIGHT, label: 'Gewicht'},
 ];
 
 function App() {
-  const [allPokemonList, setAllPokemonList] = useState<NamedAPIResourceList | null>(null);
+  const [allPokemonCount, setAllPokemonCount] = useState<number>(0);
   const [allPokemonNames, setAllPokemonNames] = useState<string[]>([]);
+  const [allPokemonIds, setAllPokemonIds] = useState<number[]>([]);
 
   const [fetchedPokemons, setFetchedPokemons] = useState<Pokemon[]>([]);
   const [filteredPokemon, setFilteredPokemon] = useState<Pokemon[]>([]);
@@ -34,7 +35,7 @@ function App() {
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const [sortingOrder, setSortingOrder] = useState<Sorting>(Sorting.ID)
+  const [sortingOrder, setSortingOrder] = useState<Sorting>(Sorting.ID);
 
   const handleSearch = async (search: string) => {
     if (!search) {
@@ -58,40 +59,41 @@ function App() {
   };
 
   const loadMorePokemons = () => {
+    setIsLoading(true);
+    fetchPokemonsById(20).then((pokemons: Pokemon[]) => {
+      pokemons.sort((a, b) => sortPokemon(a, b, sortingOrder));
+      setFetchedPokemons(pokemons);
+      setIsLoading(false);
+    });
   };
 
   const handleSortChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value as Sorting
-    setSortingOrder(value)
-  }
+    const value = event.target.value as Sorting;
+    setSortingOrder(value);
+  };
 
   useEffect(() => {
-    if (allPokemonList) {
-      setIsLoading(true);
-      fetchPokemonsById(20).then((pokemons) => {
-          pokemons.sort((a, b) => sortPokemon(a, b, sortingOrder));
-          setFetchedPokemons(pokemons);
-          setIsLoading(false);
-        },
-      );
+    if (allPokemonCount) {
+      loadMorePokemons()
     } else {
       setIsLoading(true);
-      fetchPokemonList().then((list) => {
+      fetchPokemonList().then((list: NamedAPIResourceList | null) => {
         if (list) {
+          setAllPokemonCount(list.count);
           setAllPokemonNames(list.results.map((pok) => pok.name).sort((a, b) => a.localeCompare(b)));
-          setAllPokemonList(list);
+          setAllPokemonIds(getPokemonIdRandomArray(list.results));
           setIsLoading(false);
         }
       });
     }
-  }, [allPokemonList]);
+  }, [allPokemonCount]);
 
   useEffect(() => {
     setFetchedPokemons(prev => {
-      const prevClone = [...prev]
-      prevClone.sort((a, b) => sortPokemon(a, b, sortingOrder))
-      return prevClone
-    })
+      const prevClone = [...prev];
+      prevClone.sort((a, b) => sortPokemon(a, b, sortingOrder));
+      return prevClone;
+    });
   }, [sortingOrder]);
 
 
@@ -162,11 +164,29 @@ function sortPokemon(a: Pokemon, b: Pokemon, sortingOrder: Sorting) {
   const valueA = a[sortingOrder];
   const valueB = b[sortingOrder];
 
-  if (typeof valueA === "number" && typeof valueB === "number") {
+  if (typeof valueA === 'number' && typeof valueB === 'number') {
     return valueA - valueB;
-  } else if (typeof valueA === "string" && typeof valueB === "string") {
-    return valueA.localeCompare(valueB)
+  } else if (typeof valueA === 'string' && typeof valueB === 'string') {
+    return valueA.localeCompare(valueB);
   } else return 0;
+}
+
+function getPokemonIdRandomArray(pokemonList: NamedAPIResource[]) {
+  const idList = pokemonList.map(pokemon => {
+    const match = pokemon.url.match(/\/pokemon\/(\d+)\//);
+    return match ? parseInt(match[1], 10) : null;
+  }).filter(id => id !== null);
+
+  return shuffleArray(idList);
+}
+
+function shuffleArray(array: number[]): number[] {
+  const result = [...array];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]]; // Swap
+  }
+  return result;
 }
 
 export default App;
